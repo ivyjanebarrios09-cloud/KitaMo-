c18c7d2ad16524308366849a5377891499288b3f
Ivy Jane <ivyjane033@gmail.com>
2025-12-23 18:04:03 +0000

    much more bigger size

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,8 +10,8 @@ export function Logo({
     <Image
       src="/image/logo.png"
       alt="KitaMo! Logo"
-      width={220}
-      height={56}
+      width={550}
+      height={140}
       className={cn(className)}
       priority
       {...props}
